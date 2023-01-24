897dc0c37ff69dd93ce90141927e220e964e8e09
Yuchuan <yuchuan@example.com>
2023-01-24 16:40:59 -0500

    deep dive MantinueProvider usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Text, Button, Stack, Paper, Loader } from "@mantine/core";
+import { Text, Button, Stack, Paper, Loader, MantineProvider } from "@mantine/core";
 import { ThemeProvider } from "./ThemeProvider";
 
 export default function App() {
@@ -6,12 +6,15 @@ export default function App() {
     <ThemeProvider>
       <Paper>
         <Stack align="center" mt={50}>
-          <Text  weight={500}>
+          <Text  weight={500} color="ocean-blue">
             Welcome to Mantine!
           </Text>
           <Button>First button</Button>
           <Button>Second button</Button>
           <Loader />
+          <MantineProvider theme={{ fontFamily: 'Greycliff CF, sans-serif' }}>
+            <Button>Greycliff CF button</Button>
+          </MantineProvider>
         </Stack>
       </Paper>
     </ThemeProvider>
--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -3,7 +3,22 @@ import { MantineProvider, MantineThemeOverride } from "@mantine/core";
 export const theme: MantineThemeOverride = {
   colorScheme: "dark",
   fontFamily: "Open Sans",
-  fontSizes: { md: 50 }
+  fontSizes: { md: 90 },
+  radius: { sm: 100},
+  colors: {
+    'ocean-blue': ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
+  },
+  primaryColor: 'ocean-blue',
+  components: {
+    Button: {
+      styles: (theme) => ({
+        root: {
+          backgroundColor: theme.colors['ocean-blue'][9]
+        }
+      })
+    }
+  },
+  loader: 'oval'
 };
 
 interface ThemeProviderProps {
